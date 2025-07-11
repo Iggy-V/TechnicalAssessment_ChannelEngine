0ac4c4e9c8efe2d470d527377211071161c7b330
Iggy-V <ignasvolcokas@gmail.com>
2025-07-11 14:26:14 +0300

    Add project files.

---
+++ TechnicalAssessment_ChannelEngine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechnicalAssessment_ChannelEngine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
