6e2f72dea4a7e90c2a785aac978327fa18ebce8d
Iggy-V <ignasvolcokas@gmail.com>
2025-07-12 14:12:08 +0300

    Creating DTO for orders

---
+++ TechnicalAssessment_ChannelEngine/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace TechnicalAssessment_ChannelEngine.Models
+{
+    /// Only having the necassary properties for this assignemnt there are more properties 
+    /// Check API documentation for more details
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+    }
+
+}
